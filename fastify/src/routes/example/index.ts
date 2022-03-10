3d706b8f1ea3f5cb3e50e511e848704f5b77351a
Ian <yiheng.me@gmail.com>
2022-03-10 21:10:38 +0100

    add body attr

--- fastify/src/routes/example/index.ts
+++ fastify/src/routes/example/index.ts
@@ -1,7 +1,12 @@
 import { FastifyPluginAsync } from 'fastify';
 
+interface BodyAttr {
+  attr: string;
+}
+
 const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async (req, res) => {
+  fastify.get<{ Body: BodyAttr }>('/', async (req, res) => {
+    console.log(req.body.attr);
     return 'this is an example';
   });
 };
